fcf0c94a5a3e9afda6565d4c037e691e96c6c6d6
Rishi <rishiu@gmail.com>
2021-07-21 15:59:44 +0530

    Adding authentication

--- authentication/views.py
+++ authentication/views.py
@@ -1,13 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth import login,logout,authenticate
 # Create your views here.
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 
 def loginView(request):
-    pass
+    template_name = 'authentication/login.html'
 
-def logoutView(request):
-    pass
+    if (request.method == 'POST') :
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username = username, password = password)
+        if (user is not None) :
+            login(request, user)
+            return redirect('/')
+        messages.info(request, "Invalid Credentials")
+
+    return render(request, template_name)
 
 def registerView(request):
-    pass
+    template_name = 'authentication/register.html'
+    if (request.method == 'POST') :
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if (User.objects.filter(username=username).exists()) :
+            messages.info(request, "User with this username already registered")
+            return render(request, template_name)
+
+        user = User.objects.create_user(
+            username=username, password=password, first_name=username, last_name="", email="")
+        user.save()
+        loggedIn = authenticate(username = username, password = password)
+        login(request, loggedIn)
+        return redirect('/')
+
+    return render(request, template_name)
+
+def logoutView(request):
+    logout(request)
+    return redirect('/')
--- library/urls.py
+++ library/urls.py
@@ -21,5 +21,5 @@ from django.conf import settings
 urlpatterns = [
     path('',include('store.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/',include('django.contrib.auth.urls')),
+    path('accounts/',include('authentication.urls')),
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
